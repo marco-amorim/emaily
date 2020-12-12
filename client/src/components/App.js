eca065ba8275eba2ba9dae6221cf6c4baaf8633b
marco-amorim <marcoamorim95@hotmail.com>
2020-12-12 20:04:59 -0300

    always visible header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
+import Header from './Header';
 import Landing from './Landing';
+import SurveyNew from './SurveyNew';
 
 const App = () => {
 	return (
 		<div>
 			<BrowserRouter>
 				<div>
+					<Header />
 					<Route path="/" exact component={Landing} />
-					<Route path="/surveys" component={Dashboard} />
+					<Route path="/surveys" exact component={Dashboard} />
+					<Route path="/surveys/new" component={SurveyNew} />
 				</div>
 			</BrowserRouter>
 		</div>
