a77429e27f2d5d05061096cddb4f7b7976976d3c
marco-amorim <marcoamorim95@hotmail.com>
2020-12-12 20:00:52 -0300

    matching routes with exact

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
 import Landing from './Landing';
 
 const App = () => {
@@ -7,7 +8,8 @@ const App = () => {
 		<div>
 			<BrowserRouter>
 				<div>
-					<Route path="/" component={Landing} />
+					<Route path="/" exact component={Landing} />
+					<Route path="/surveys" component={Dashboard} />
 				</div>
 			</BrowserRouter>
 		</div>
