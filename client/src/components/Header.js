8cc1a62de6e039e9cf0eb0746aece480c4d78aa6
marco-amorim <marcoamorim95@hotmail.com>
2020-12-12 20:16:47 -0300

    setting up materialize CSS with webpack

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Header = () => {
-	return <div>Header!</div>;
-};
+class Header extends Component {
+	render() {
+		return <div>Header!</div>;
+	}
+}
 
 export default Header;
