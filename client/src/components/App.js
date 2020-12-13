2eb456034560218c5785c6bcf3ad6558ecdac20d
marco-amorim <marcoamorim95@hotmail.com>
2020-12-12 21:02:40 -0300

    setting up actions

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Header from './Header';
 import Landing from './Landing';
 import SurveyNew from './SurveyNew';
+import * as actions from '../actions';
+import { connect } from 'react-redux';
 
-const App = () => {
-	return (
-		<div>
-			<BrowserRouter>
-				<div>
-					<Header />
-					<Route path="/" exact component={Landing} />
-					<Route path="/surveys" exact component={Dashboard} />
-					<Route path="/surveys/new" component={SurveyNew} />
-				</div>
-			</BrowserRouter>
-		</div>
-	);
-};
+class App extends Component {
+	componentDidMount() {
+		this.props.fetchUser();
+	}
 
-export default App;
+	render() {
+		return (
+			<div className="container">
+				<BrowserRouter>
+					<div>
+						<Header />
+						<Route path="/" exact component={Landing} />
+						<Route path="/surveys" exact component={Dashboard} />
+						<Route path="/surveys/new" component={SurveyNew} />
+					</div>
+				</BrowserRouter>
+			</div>
+		);
+	}
+}
+
+export default connect(null, actions)(App);
