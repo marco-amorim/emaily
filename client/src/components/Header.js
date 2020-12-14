81739c3ac3d7138a259809515ffd2509c7678fe6
marco-amorim <marcoamorim95@hotmail.com>
2020-12-14 07:39:38 -0300

    accessing global state in the header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return 'Loading';
+			case false:
+				return 'Logged out';
+
+			default:
+				return 'Logged in';
+		}
+	}
+
 	render() {
 		return (
 			<nav>
@@ -9,6 +22,7 @@ class Header extends Component {
 						Emaily
 					</a>
 					<ul className="right">
+						{this.renderContent()}
 						<li>
 							<a href="/">Login with Google</a>
 						</li>
@@ -19,4 +33,8 @@ class Header extends Component {
 	}
 }
 
-export default Header;
+function mapStateToProps(state) {
+	return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(Header);
