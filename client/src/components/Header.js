97ae9144a6801d84a5f401fdf7996b49be827c98
marco-amorim <marcoamorim95@hotmail.com>
2020-12-21 20:21:36 -0300

    basic styling on header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -33,10 +33,11 @@ class Header extends Component {
 	render() {
 		return (
 			<nav>
-				<div className="nav-wrapper">
+				<div className="nav-wrapper green">
 					<Link
 						to={this.props.auth ? '/surveys' : '/'}
 						className="left brand-logo"
+						style={{ marginLeft: '10px' }}
 					>
 						Emaily
 					</Link>
