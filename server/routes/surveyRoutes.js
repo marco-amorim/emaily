0019051cf655bfe94bda320d4a5ca050369d33fd
marco-amorim <marcoamorim95@hotmail.com>
2020-12-25 23:54:15 -0300

    creating surveys

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,8 +1,17 @@
+const mongoose = require('mongoose');
 const requireCredits = require('../middlewares/requireCredits');
 const requireLogin = require('../middlewares/requireLogin');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = (app) => {
 	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
-		res.send({ message: 'survey cration route' });
+		const { title, subject, body, recipients } = req.body;
+
+		const survey = new Survey({
+			title,
+			subject,
+			body,
+		});
 	});
 };
