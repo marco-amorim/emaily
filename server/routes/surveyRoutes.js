e5a190d3c7cfc185edb296daf8b9eaa48153512f
marco-amorim <marcoamorim95@hotmail.com>
2021-01-05 10:13:07 -0300

    fix typo on user credits property

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -30,7 +30,7 @@ module.exports = (app) => {
 		try {
 			await mailer.send();
 			await survey.save();
-			req.user.credites -= 1;
+			req.user.credits -= 1;
 			const user = await req.user.save();
 
 			res.send(user);
