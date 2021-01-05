083fafa2db58b76cf54cae0e6956e810ab8de940
marco-amorim <marcoamorim95@hotmail.com>
2021-01-05 09:44:21 -0300

    fixing property names

--- server/client/src/components/surveys/formFields.js
+++ server/client/src/components/surveys/formFields.js
@@ -1,8 +1,8 @@
 const FIELDS = [
-	{ label: 'Survey Title', name: 'surveyTitle' },
+	{ label: 'Survey Title', name: 'title' },
 	{ label: 'Subject Line', name: 'subject' },
 	{ label: 'Email Body', name: 'body' },
-	{ label: 'Recipients List', name: 'emails' },
+	{ label: 'Recipients List', name: 'recipients' },
 ];
 
 export default FIELDS;
