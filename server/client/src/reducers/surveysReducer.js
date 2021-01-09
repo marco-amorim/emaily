0b934e9d405f014789164626f7ce23fac5bcf4d2
marco-amorim <marcoamorim95@hotmail.com>
2021-01-09 07:59:40 -0300

    fetching list of surveys to the dashboard

---
+++ server/client/src/reducers/surveysReducer.js
@@ -0,0 +1,13 @@
+import { FETCH_SURVEYS } from '../actions/types';
+
+const surveysReducer = (state = [], action) => {
+	switch (action.type) {
+		case FETCH_SURVEYS:
+			return action.payload;
+
+		default:
+			return state;
+	}
+};
+
+export default surveysReducer;
